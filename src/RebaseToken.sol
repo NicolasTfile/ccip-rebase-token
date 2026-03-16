77b90f74e1ea3d24ab044a0c849fb8a48305f63d
NicolasTfile <nicolastfile@gmail.com>
2026-03-16 12:15:52 +0100

    wrote setInterestRate function for Rebase Token project

--- src/RebaseToken.sol
+++ src/RebaseToken.sol
@@ -4,6 +4,29 @@ pragma solidity ^0.8.24;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+/**
+ * @title RebaseToken
+ * @author Olaniyi Agunloye
+ * @notice This is a cross-chain rebase token that incentivises users to deposit into a vault and gain interest in rewards
+ * @notice The interest rate in the smart contract can only decrease
+ * @notice Each user will have their own interest rate that is the global interest rate at the time of depositing
+ */
 contract RebaseToken is ERC20 {
+    error RebaseToken__InterestRateCanOnlyDecrease(uint256 oldInterestRate, uint256 newInterestRate);
+
+    uint256 private s_interestRate = 5e10;
+
+    event InterestRateSet(uint256 newInterestRate);
+
     constructor() ERC20("RebaseToken", "RBT") {}
+
+    function setInterestRate(uint256 _newInterestRate) external {
+        // Logic to set the new interest rate, ensuring it can only decrease
+        if (_newInterestRate < s_interestRate) {
+            revert RebaseToken__InterestRateCanOnlyDecrease(s_interestRate, _newInterestRate);
+        }
+
+        s_interestRate = _newInterestRate;
+        emit InterestRateSet(_newInterestRate);
+    }
 }
