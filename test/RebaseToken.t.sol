795e84ebce5e2b0602b945616f2f0a76df3ba02c
NicolasTfile <nicolastfile@gmail.com>
2026-03-18 13:22:10 +0100

    Testing rebase token project

---
+++ test/RebaseToken.t.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {Test, console} from "forge-std/Test.sol";
+import {RebaseToken} from "../src/RebaseToken.sol";
+import {Vault} from "../src/Vault.sol";
+import {IRebaseToken} from "../src/interfaces/IRebaseToken.sol";
+
+contract RebaseTokenTest is Test {
+    RebaseToken private rebaseToken;
+    Vault private vault;
+
+    function setUp() public {
+        // Deploy the RebaseToken contract
+        rebaseToken = new RebaseToken();
+        // Deploy the Vault contract, passing the address of the RebaseToken contract
+        vault = new Vault(IRebaseToken(address(rebaseToken)));
+    }
+}
