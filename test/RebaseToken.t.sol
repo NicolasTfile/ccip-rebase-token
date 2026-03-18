72bc3d99ae1eb3315bb249503a2cebd5b30ae35d
NicolasTfile <nicolastfile@gmail.com>
2026-03-18 13:47:42 +0100

    wrote a test for linear deposit

--- test/RebaseToken.t.sol
+++ test/RebaseToken.t.sol
@@ -11,10 +11,30 @@ contract RebaseTokenTest is Test {
     RebaseToken private rebaseToken;
     Vault private vault;
 
+    address public owner = makeAddr("owner");
+    address public user1 = makeAddr("user1");
+
     function setUp() public {
+        vm.startPrank(owner);
         // Deploy the RebaseToken contract
         rebaseToken = new RebaseToken();
         // Deploy the Vault contract, passing the address of the RebaseToken contract
         vault = new Vault(IRebaseToken(address(rebaseToken)));
+        rebaseToken.grantMintAndBurnRole(address(vault));
+        (bool success,) = payable(address(vault)).call{value: 1e18}("");
+        vm.stopPrank();
+    }
+
+    function testDepositLinear(uint256 amount) public {
+        vm.assume(amount > 1e5);
+        amount = bound(amount, 1e5, type(uint96).max);
+        // 1. Deposit
+        vm.startPrank(user1);
+        vm.deal(user1, amount);
+        // 2. Check our rebase token balance
+        // 3. Warp the time and check the balance again
+        // 4. Warp the time and check the balance again
+
+        vm.stopPrank();
     }
 }
